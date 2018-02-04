51d30ee934a2f54479289e86b3b560ebd13e8958
Rafizan Uda Baharum <rafizan.baharum@canang.com.my>
2018-02-05 00:06:27 +0800

    Add examples

--- function_example.ts
+++ function_example.ts
@@ -1,6 +1,8 @@
 		
 // function declaration
+// with no return value
 function calculate():void {
+	// function body
 	let someArray:number[] = [1, 2, 3];
 	for (let entry of someArray) {
 		console.log(entry); 
@@ -10,8 +12,16 @@ function calculate():void {
 // calling function
 calculate();
 
+// function declaration
+// with return value
+function getMessage():string {
+	// function body
+	let message:string = 'this ' + ' is' + ' a' + ' test';
+	return message;
+}
 
-document.body.innerHTML = '';
+
+document.body.innerHTML = getMessage();
 
 
 
