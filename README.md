6c3eb09dd803ea25dcf3749aaa0a6247a70a14c2
Rafizan Uda Baharum <rafizan.baharum@canang.com.my>
2018-02-04 23:35:41 +0800

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # terang-typescript
+
 Typescript Test
+
+
+npm install -g typescript
+tsc test.ts
+
+open test.html using browser
---
+++ test.ts
@@ -0,0 +1,17 @@
+		
+let s:string = 'something';
+console.log('test');	
+calculate();
+
+
+
+function calculate():void {
+	let someArray:number[] = [1, 2, 3];
+	for (let entry of someArray) {
+		console.log(entry); 
+	}
+}
+
+
+document.body.innerHTML = s;
+
