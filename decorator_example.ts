fd450e06f2399930931e34c82fc028ce6b35f618
Rafizan Uda Baharum <rafizan.baharum@canang.com.my>
2018-02-05 23:21:19 +0800

    Add example

--- decorator_example.ts
+++ decorator_example.ts
@@ -1,27 +1,8 @@
 function Log(target: any) {
-
-    // save a reference to the original constructor
-    var original = target;
-
-    // a utility function to generate instances of a class
-    function construct(constructor, args) {
-        var c : any = function () {
-            return constructor.apply(this, args);
-        }
-        c.prototype = constructor.prototype;
-        return new c();
-    }
-
     // the new constructor behaviour
     var f : any = function (...args) {
-        console.log("New: " + original.name);
-        return construct(original, args);
+        console.log("New: " + target.name);
     }
-
-    // copy prototype so intanceof operator still works
-    f.prototype = original.prototype;
-
-    // return new constructor (will override original)
     return f;
 }
 
