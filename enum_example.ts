e05599747ea4d404402de18588c6888b5ae85b7c
Rafizan Uda Baharum <rafizan.baharum@canang.com.my>
2018-02-05 22:26:07 +0800

    Add enum example

---
+++ enum_example.ts
@@ -0,0 +1,18 @@
+// enum declaration
+enum Brand {
+    PROTON,
+    HONDA,
+    TOYOTA,
+    SUZUKI
+}
+
+let myBrand: Brand;
+myBrand = Brand.HONDA;
+
+// calling function
+document.body.innerHTML = myBrand.toString();
+
+
+
+
+
